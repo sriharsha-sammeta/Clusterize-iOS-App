8a628eeff22beee395bd3aa7f91ee3739fcac69b
Sriharsha Sammeta <sriharsha@Sriharsha-MacBook-Pro.local>
2015-03-21 15:50:03 +0530

    Initial Commit

---
+++ ClusterizeTests/ClusterizeTests.swift
@@ -0,0 +1,36 @@
+//
+//  ClusterizeTests.swift
+//  ClusterizeTests
+//
+//  Created by Sriharsha Sammeta on 21/03/15.
+//  Copyright (c) 2015 Sriharsha Sammeta. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ClusterizeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
